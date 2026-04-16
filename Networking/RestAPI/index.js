7854cea343113d27bc1273a136ab0a22bbee1b2a
Santhoshvenkidusamy <santhoshvenkidusamy@gmail.com>
2026-04-16 21:12:57 +0530

    REST API

--- Networking/RestAPI/index.js
+++ Networking/RestAPI/index.js
@@ -1,13 +1,72 @@
 import express from 'express';
-
+import bodyParser from 'body-parser';
+import e from 'express';
 const app = express();
+app.use(bodyParser.json()); 
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
-
-app.get('/', (req, res) => {
-    console.log("Request",req);
-    console.log("Response",res);
+app.all('/', (req, res) => {
+    // console.log("Request",req);
+    // console.log("Response",res);
     res.send("I'm alive!");
 });
+
+ const todos = [
+        { id: "1", title: 'Buy groceries', completed: false },
+        { id: "2", title: 'Walk the dog', completed: true },
+        { id: "3", title: 'Finish homework', completed: false }
+    ];
+
+// READ
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+//CREATE
+app.post('/todos', (req, res) => {
+   const newTodo = req.body
+   todos.push(newTodo);
+   res.json({
+    message: "Todo created successfully"
+   });
+});
+
+//UPDATE
+app.put('/todos/:id',(req,res)=>{
+    const newTodo = req.body;
+    const todoIndex = todos.findIndex(todo => todo.id === req.params.id);
+    if(todoIndex !== -1){
+        todos[todoIndex] = {
+            id: req.params.id,
+            ...newTodo
+        }
+        res.json({
+            message: "Todo updated successfully"
+        });
+    }
+    else{
+       res.status(404).json({
+        message: "Todo not found"
+    });
+}
+        
+    
+})
+//DELETE
+
+app.delete('/todos/:id',(req,res)=>{
+    const todoIndex = todos.findIndex(todo => todo.id === req.params.id);
+    if(todoIndex !== -1){
+        todos.splice(todoIndex,1);
+        res.json({
+            message: "Todo deleted successfully"
+        });
+    }
+    else{
+        res.status(404).json({
+            message: "Todo not found"
+        });
+    }
+})
